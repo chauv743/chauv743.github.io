22f497e56f3506208c0fabb57a4d95759305fa11
Vicky Chau <vickychau@Vickys-MBP.wireless.newschool.edu>
2015-12-18 09:29:06 -0500

    updated js file

--- timepiece/sketch.js
+++ timepiece/sketch.js
@@ -7,18 +7,30 @@ var circleScale5;
 var maxScale = 1600;
 var maxScale2 = 600;
 var maxScale3 = 400;
+var move = 0;
+var spongebob;
+var backgroundLines;
+var audio = new Audio('spongebob.mp3');
+
 
 /**
  * Setup the sketch
+
  */
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+
+//playing the audio file
+audio.play();
 
 	// set our scale to 1 for starters
 	circleScale = 1;
 	circleScale2 = 1;
 	circleScale3 = 1;
 	circleScale4 = 1;
+
 
 }
 
@@ -29,12 +41,47 @@ function setup() {
 function draw() {
 	// set background to black
 	background(0);
-	stroke(88, 179, 183);
-	strokeWeight(2);
-	noFill();
+
+// background lines color
+	stroke(127, 125, 125);
+	strokeWeight(1);
+	point(width/2, height/2);
+
+
+// background lines rotation
+	var angle = 360/12;
+	push();
+	//changing angle to degrees instead of radians
+	angleMode(DEGREES);
+	//translating the canvas from 0,0 (far left) to center
+	translate(width/2, height/2);
+	//rotates the entire set of lines
+	rotate(move);
+	for(var i = 0; i<12; i++) {
+		rotate(angle);
+		line(0,0,width,0);
+	}
+
+	// line(0, 0, 2000, 1000);
+	// line(0, 630, 19000, -8500);
+	// line(640, 0, 640, 1000);
+	// line(0, 320, 1500, 320);
+	// line(400, 0, 1150, 1000);
+	// line(0, 1000, 8950, -8500);
+	pop();
+
+	move = move + 1;
+
+
+
 
 	// try commenting out these different functions
 	// to see two different approaches
+
+//circles
+	stroke(88, 179, 183);
+	strokeWeight(2);
+	noFill();
 
 	drawCircleUsingRadius();
 
@@ -68,6 +115,7 @@ if(circleScale4 > maxScale){
 }
 
 }
+
 
 
 /**
